6bcba83ae8e0dea7cecb842a59a68c438788dbfc
giulir1 <83610835+giulir1@users.noreply.github.com>
2022-05-23 19:06:31 -0300

    Add files via upload

---
+++ Ejercicio1/main.py
@@ -0,0 +1,8 @@
+from manejadorFacultades import manejadorFacultades
+from facultad import facultad
+
+
+if __name__ == '__main__':
+    control = manejadorFacultades()
+    #control.leerArchivo()
+    unaFacultad = facultad(1,'Facultad de Ciencias Exactas, Fisicas y Naturales', 'Av. Ignacio De La Roza 590(O)', 'Rivadavia', '2644260353')
